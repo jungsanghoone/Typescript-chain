509adc8b95932f156377598329bcf3d7eaaa1935
sanghoone <sanghoone.jung89@gmail.com>
2021-01-06 21:19:03 +0900

    init: Types in Typescript

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const sayHi = (name, age, gender) => {
+    return `Hello ${name} ,you age ${age} , you are a ${gender}!`;
+};
+sayHi("sanghoone", 33, "hi");
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+const sayHi = (name: string, age: number, gender: string): string => {
+    return `Hello ${name} ,you age ${age} , you are a ${gender}!`
+}
+
+sayHi("sanghoone", 33, "hi");
+export {}
